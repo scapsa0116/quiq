82194bb3cbbce66972a006d1086ee7d84b725c11
stela <stela.capsa@gmail.com>
2021-08-16 23:40:08 -0500

    functions build

--- src/registration/Register.js
+++ src/registration/Register.js
@@ -5,13 +5,23 @@ function Register() {
     const [user, setUser] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [passwordError, setpasswordError] = useState('');
+    const [passwordError, setPasswordError] = useState('');
     const [emailError, setEmailError] = useState('');
     const [hasAccount, sethasAccount] = useState('');
 
 
+    const clearImputs = () => {
+        setEmail("")
+        setPassword("")
+    }
+
+    const clearErrors = () => {
+        setEmailError("")
+        setPaswordErrors("")
+    }
 
 const handelLogin = () => {
+    clearErrors()
     fire
     .auth()
     .signInWithEmailAndPassword(email, password)
@@ -20,9 +30,9 @@ const handelLogin = () => {
             case "auth/invalid-email":
                 case "auth/user-disabeled":
                     case "auth/user-not-found":
-                        setEmaiError(err.message);
+                        setEmailError(err.message);
                         break;
-                        case "auth/wrong-password":
+                        case "auth/weak-password":
                             setPasswordError(err.message);
                             breack;
         }
@@ -30,6 +40,44 @@ const handelLogin = () => {
 }
 
 
+const handleSignup = () => {
+    clearErrors();
+    fire
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .catch(err => {
+        switch(err.code) {
+            case "auth/email-already-in-use":
+                case "auth/invalid-email":
+                        setEmailError(err.message);
+                        break;
+                        case "auth/wrong-password":
+                            setPasswordError(err.message);
+                            breack;
+        }
+    })
+
+}
+
+const handleLogout =() => {
+    fire.auth().signOut();
+}
+
+const authListener = () => {
+    fire.auth().onAuthStateChanged((user) => {
+        if (user) {
+            clearImputs();
+            setUser(user);
+        } else {
+            setUser("")
+        }
+    })
+}
+
+
+useEffect(() => {
+    authListener();
+}, [])
 
     return (
         <div>
