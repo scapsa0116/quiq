59948398b37835f73c04676b9495aca900ae5e59
stela <stela.capsa@gmail.com>
2021-08-16 22:47:34 -0500

    creating the form

--- src/registration/Register.js
+++ src/registration/Register.js
@@ -1,6 +1,36 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import fire from '../fire'
 
 function Register() {
+    const [user, setUser] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [passwordError, setpasswordError] = useState('');
+    const [emailError, setEmailError] = useState('');
+    const [hasAccount, sethasAccount] = useState('');
+
+
+
+const handelLogin = () => {
+    fire
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .catch(err => {
+        switch(err.code) {
+            case "auth/invalid-email":
+                case "auth/user-disabeled":
+                    case "auth/user-not-found":
+                        setEmaiError(err.message);
+                        break;
+                        case "auth/wrong-password":
+                            setPasswordError(err.message);
+                            breack;
+        }
+    })
+}
+
+
+
     return (
         <div>
             hiii
